3dede118641a8fdc1b701641fa16c8004672137a
César <424321999@qq.com>
2018-11-14 09:03:16 +0800

    Initial Commit

---
+++ LightDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LightDemo
+//
+//  Created by user on 2018/11/14.
+//  Copyright © 2018年 adu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LightDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LightDemo
+//
+//  Created by user on 2018/11/14.
+//  Copyright © 2018年 adu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
